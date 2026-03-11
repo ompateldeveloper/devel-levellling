d5a49bb4d4910a1eaa34c7a3aae103dd082ac169
ompateldeveloper <ompateldeveloper@gmail.com>
2026-03-11 23:59:24 +0530

    micro fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,8 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 export const auth = betterAuth({
+    baseURL: process.env.BETTER_AUTH_URL,
+    trustedOrigins: [process.env.BETTER_AUTH_URL!],
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
